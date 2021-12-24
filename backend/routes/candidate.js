b61a045f8827c728c0a0d44107512116005c901c
Gokulnath D <imgnath28@gmail.com>
2021-12-24 17:39:38 +0530

    Update and Delete Candidate API

--- backend/routes/candidate.js
+++ backend/routes/candidate.js
@@ -1,6 +1,6 @@
 
 const express = require('express');
-
+const mongoose = require('mongoose');
 const { Candidate } = require('../models/candidate.js');
 
 const candidateRouter = express.Router();
@@ -8,6 +8,11 @@ const candidateRouter = express.Router();
 candidateRouter.get(`/`, async (req, res) => {
     const candidateList = await Candidate.find({});
     res.send(candidateList);
+});
+
+candidateRouter.get(`/:id`, async (req, res) => {
+    const candidate = await Candidate.findById(req.params.id);
+    res.send(candidate);
 });
 
 candidateRouter.post(`/`, async (req, res) => {
@@ -32,6 +37,50 @@ candidateRouter.post(`/`, async (req, res) => {
             success: false
         })
     }
+});
+
+candidateRouter.put(`/:id`, async (req, res) => {
+    if (!mongoose.isValidObjectId(req.params.id)) {
+        return res.status(400).send('Invalid Candidate Id');
+    }
+
+    const candidate = await Candidate.findById(req.params.id);
+    if(!candidate) return res.status(400).send('Invalid Candidate');
+
+    const updatedCandidate = await Candidate.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            phone: req.body.phone,
+            email: req.body.email,
+            status: req.body.status,
+            date: req.body.date,
+            aadhar: req.body.aadhar,
+            pan: req.body.pan,
+        },
+        { new: true }
+    );
+
+    if(!updatedCandidate) {
+        return res.status(400).send('Candidate not Found!')
+    }
+
+    res.send(updatedCandidate);
+});
+
+candidateRouter.delete(`/:id`, (req, res) => {
+
+    Candidate.findByIdAndRemove(req.params.id)
+    .then((deletedCandidate) => {
+        if(deletedCandidate) {
+            return res.status(201).json({ success: true, message: 'Deleted Successfully' })
+        } else {
+            return res.status(404).json({ success: false, message: 'Candidate not found' })
+        }
+    })
+    .catch((err) => {
+        return res.status(400).json({ success: false, error: err })
+    })
 });
 
 module.exports = candidateRouter;
